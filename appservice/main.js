2f4339cadfa89a400e11681c3abae508bf365649
Kegan Dougal <kegan@matrix.org>
2015-10-05 15:53:51 +0100

    Add basic appservice structure with config file

--- appservice/main.js
+++ appservice/main.js
@@ -1,33 +1,90 @@
+var Cli = require("matrix-appservice-bridge").Cli;
+var Bridge = require("matrix-appservice-bridge").Bridge;
+var AppServiceRegistration = require("matrix-appservice-bridge").AppServiceRegistration;
 var Purple = require('../purple');
-var read = require("read");
-var username, password, protocol;
 
-var p = new Purple({
-    debug: true,
-});
+var REGISTRATION_PATH = "purple-registration.yaml";
+var SCHEMA_PATH = "purple-config-schema.yaml";
 
-var Status = require('../lib/status');
-var Account = require('../lib/account');
-
-p.protocols.forEach(function(prpl, i) {
-    console.log("[%s] %s", i, prpl.name);
-});
-
-read({ prompt: "Service number: " }, function(err, srv) {
-    if (err) { console.error(err); process.exit(1); }
-    protocol = p.protocols[parseInt(srv)].id;
-    read({ prompt: "Username: " }, function(err, uname) {
-        if (err) { console.error(err); process.exit(1); }
-        username = uname;
-        read({ prompt: "Password: ", silent: true }, function(err, pword) {
-            if (err) { console.error(err); process.exit(1); }
-            password = pword;
-            main();
+var cli = new Cli({
+    registrationPath: REGISTRATION_PATH,
+    bridgeConfig: {
+        affectsRegistration: true,
+        schema: SCHEMA_PATH,
+        defaults: {
+            bot_username: "purplebridge",
+            user_prefixes: {}
+        }
+    },
+    generateRegistration: function(reg, callback) {
+        var cfg = cli.getConfig();
+        reg.setHomeserverToken(AppServiceRegistration.generateToken());
+        reg.setAppServiceToken(AppServiceRegistration.generateToken());
+        reg.setSenderLocalpart(cfg.bot_username);
+        var prefixes = {};
+        cfg.accounts.forEach(function(acc) {
+            if (cfg.user_prefixes[acc.protocol]) {
+                prefixes[cfg.user_prefixes[acc.protocol]] = true;
+            }
+            else {
+                prefixes[acc.protocol] = true;
+            }
         });
-    });
-});
+        Object.keys(prefixes).forEach(function(prefix) {
+            reg.addRegexPattern("users", "@" + prefix + "_.*", true);
+        });
+        callback(reg);
+    },
+    run: function(port, config) {
+        console.log("Run run run %s", config);
+        /*
+        bridge = new Bridge({
+            homeserverUrl: config.homserver.url,
+            domain: config.homeserver.server_name,
+            registration: REGISTRATION_PATH,
 
-function main() {
+            controller: {
+                onUserQuery: function(queriedUser) {
+                    return {}; // auto-provision users with no additonal data
+                },
+
+                onEvent: function(request, context) {
+                    var event = request.getData();
+                    if (event.type !== "m.room.message" || !event.content || event.room_id !== ROOM_ID) {
+                        return;
+                    }
+                    requestLib({
+                        method: "POST",
+                        json: true,
+                        uri: SLACK_WEBHOOK_URL,
+                        body: {
+                            username: event.user_id,
+                            text: event.content.body
+                        }
+                    }, function(err, res) {
+                        if (err) {
+                            console.log("HTTP Error: %s", err);
+                        }
+                        else {
+                            console.log("HTTP %s", res.statusCode);
+                        }
+                    });
+                }
+            }
+        });
+        console.log("Matrix-side listening on port %s", port);
+        bridge.run(port, config); */
+    }
+})
+
+cli.run();
+
+function runPurple() {
+    var p = new Purple({
+        debug: true,
+    });
+    var Status = require('../lib/status');
+    var Account = require('../lib/account');
     var account = new Account(username, protocol);
     account.password = password;
 
