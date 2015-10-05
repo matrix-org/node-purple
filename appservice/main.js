fee6ed1d0f159085a94e48d7aebb79a75b17e2de
Kegan Dougal <kegan@matrix.org>
2015-10-05 14:22:19 +0100

    Add appservice directory and add stdin for protocol/uname/pass

---
+++ appservice/main.js
@@ -0,0 +1,47 @@
+var Purple = require('../purple');
+var Status = require('../lib/status');
+var Account = require('../lib/account');
+var read = require("read");
+var username, password, protocol;
+
+var p = new Purple({
+    debug: true,
+});
+
+p.protocols.forEach(function(prpl, i) {
+    console.log("[%s] %s", i, prpl.name);
+});
+
+read({ prompt: "Service number: " }, function(err, srv) {
+    if (err) { console.error(err); process.exit(1); }
+    protocol = p.protocols[parseInt(srv)].id;
+    read({ prompt: "Username: " }, function(err, uname) {
+        if (err) { console.error(err); process.exit(1); }
+        username = uname;
+        read({ prompt: "Password: ", silent: true }, function(err, pword) {
+            if (err) { console.error(err); process.exit(1); }
+            password = pword;
+            main();
+        });
+    });
+});
+
+function main() {
+    var account = new Account(username, protocol);
+    account.password = password;
+
+    p.enableAccount(account);
+
+    var s = new Status('Testing App', Status.AVAILABLE);
+    p.savedstatus = s;
+
+    p.on('create_conversation', function (conversation) {
+        console.log("we have a new conversation", conversation.title, conversation.name);
+    });
+
+    p.on('write_conv', function (conv, who, alias, message, flags, time) {
+        if (alias !== username && who !== username) {
+            conv.send('you said "' + message + '"');
+        }
+    });
+}
