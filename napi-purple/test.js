ebc9d583a626d4c8bf13e02301ef962a4883a5e3
Will Hunt <will@half-shot.uk>
2018-10-21 22:01:29 +0100

    Test more things

--- napi-purple/test.js
+++ napi-purple/test.js
@@ -1,2 +1,16 @@
 const libpurple = require('./build/Release/module');
-console.log(`Libpurple core version:`, libpurple.purple_core_get_version());
+console.log(libpurple);
+libpurple.debug.set_enabled(1);
+console.log(`Libpurple core version:`, libpurple.core.get_version());
+
+function testCreateAndQuit() {
+    libpurple.core.init();
+    console.log("Created new purple core");
+    setTimeout(() => {
+        libpurple.core.quit();
+        console.log("Quit purple core");
+    }, 2000);
+}
+
+
+testCreateAndQuit();
