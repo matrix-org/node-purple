831d4677ba028d865d9ec362aaa642dbfe5d13bf
Will Hunt <will@half-shot.uk>
2018-10-29 15:31:31 +0000

    Remove glib stuff

--- napi-purple/src/helper.c
+++ napi-purple/src/helper.c
@@ -15,6 +15,7 @@ void getSetupPurpleStruct(napi_env env, napi_callback_info info, s_setupPurple *
     s_setupPurple stemp;
     napi_valuetype type;
     napi_value value;
+    napi_ref ref;
     stemp.userDirSet = false;
 
     napi_get_cb_info(env, info, &argc, &opts, NULL, NULL);
@@ -48,7 +49,6 @@ void getSetupPurpleStruct(napi_env env, napi_callback_info info, s_setupPurple *
 napi_value setupPurple(napi_env env, napi_callback_info info) {
     napi_value n_undef;
     s_setupPurple opts;
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
     PurpleConversationUiOps uiopts = {
       NULL,                      /* create_conversation  */
       NULL,                      /* destroy_conversation */
@@ -74,6 +74,10 @@ napi_value setupPurple(napi_env env, napi_callback_info info) {
 
     PurpleEventLoopUiOps eventLoop;
     eventLoop_get(&eventLoop, &env);
+
+    printf("purple_eventloop_set_ui_ops()\n");
+    purple_eventloop_set_ui_ops(&eventLoop);
+
     getSetupPurpleStruct(env, info, &opts);
     if (opts.userDirSet) {
   	  purple_util_set_user_dir(opts.userDir);
@@ -85,11 +89,8 @@ napi_value setupPurple(napi_env env, napi_callback_info info) {
 
     printf("purple_conversation_set_ui_ops()\n");
     purple_conversation_set_ui_ops(&uiopts, NULL);
-    printf("purple_eventloop_set_ui_ops()\n");
-    purple_eventloop_set_ui_ops(&eventLoop);
     printf("purple_core_init()\n");
     purple_core_init("matrix-bridge");
-    g_main_loop_run(loop);
     return n_undef;
 }
 
