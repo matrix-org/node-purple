05eeeb2e29b083f08837045a510923cec2832d16
Will Hunt <will@half-shot.uk>
2018-10-31 18:39:49 +0000

    Test event polling

--- test.js
+++ test.js
@@ -2,15 +2,22 @@ const libpurple = require('./build/Debug/module');
 console.log(`Libpurple core version:`, libpurple.core.get_version());
 console.log(libpurple);
 
-setTimeout(() => {
-    libpurple.core.quit();
-}, 2000);
+setInterval(() => {
+    libpurple.helper.pollEvents().forEach((ev) => {
+        console.log("Got event:", ev);
+    });
+}, 100);
 
 libpurple.helper.setupPurple(
     {
-        debugEnabled: 0
+        debugEnabled: 1,
     }
 );
 console.log("Finished setting up purple!");
 //console.log("Plugin list:", libpurple.plugins.get_protocols());
-libpurple.accounts.new("TestJabber", "prpl-jabber")
+const acct = libpurple.accounts.find("halfshot@localhost/", "prpl-jabber");
+console.log("Acct:", acct);
+//console.log("Enabled:", libpurple.accounts.get_enabled(acct.handle));
+//libpurple.accounts.set_enabled(acct.handle, true);
+//console.log("Enabled:", libpurple.accounts.get_enabled(acct.handle));
+//libpurple.accounts.new("TestJabber", "prpl-jabber");
