311f3d7815f0e4aaedc5daef40674e1b78ff8b89
Will Hunt <will@half-shot.uk>
2018-11-04 13:18:03 +0000

    Add buddy bindings

--- src/module.c
+++ src/module.c
@@ -6,11 +6,15 @@
 #include "bindings/b_core.h"
 #include "bindings/b_plugins.h"
 #include "bindings/b_accounts.h"
+#include "bindings/b_buddy.h"
 #include "helper.h"
 #include "messaging.h"
 
 
 napi_value Init(napi_env env, napi_value exports) {
+  // Future binding should expose a XXX_bind_node(env,exports) function
+  // and apply their exported functions to that rather than use a header.
+  // The stuff below is mostly wrong.
   napi_value _fn_purple_core_get_version;
   napi_value _fn_purple_core_init;
   napi_value _fn_purple_core_quit;
@@ -99,36 +103,12 @@ napi_value Init(napi_env env, napi_value exports) {
   napi_set_named_property(env, ns_messaging, "sendIM", _func);
 
   napi_set_named_property(env, exports, "messaging", ns_messaging);
+
+  // This is the right way to do it :)
+  buddy_bind_node(env, exports);
+
   return exports;
 }
-
-
-// napi_value MyFunction(napi_env env, napi_callback_info info) {
-//   napi_status status;
-//   size_t argc = 1;
-//   int number = 0;
-//   napi_value argv[1];
-//   status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
-//
-//   if (status != napi_ok) {
-//     napi_throw_error(env, NULL, "Failed to parse arguments");
-//   }
-//
-//   status = napi_get_value_int32(env, argv[0], &number);
-//
-//   if (status != napi_ok) {
-//     napi_throw_error(env, NULL, "Invalid number was passed as argument");
-//   }
-//   napi_value myNumber;
-//   number = number * 2;
-//   status = napi_create_int32(env, number, &myNumber);
-//
-//   if (status != napi_ok) {
-//     napi_throw_error(env, NULL, "Unable to create return value");
-//   }
-//
-//   return myNumber;
-// }
 
 
 NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
